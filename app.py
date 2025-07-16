4d4dc67a5d890ebe748a93ab5543c6947ae4993b
BeyondAnalysIs <anderson.ing.24@gmail.com>
2025-07-16 15:51:29 +0200

    primer commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime
+
+# Configuración de la página
+
+st.set_page_config(
+    page_title='Dashboard '
+)
+ 
